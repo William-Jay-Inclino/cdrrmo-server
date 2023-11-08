a37937880a2fd3215558e699eae765353c13033e
William Jay Inclino <wjay.inclino@gmail.com>
2023-11-08 16:25:56 +0800

    ref #002: create dto for create and update user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,62 @@
-export class CreateUserDto {}
+import { DispatchStatusEnum, GenderEnum, UserLevelEnum, UserStatusEnum, UserTypeEnum } from '@prisma/client';
+import { IsEnum, IsString, IsDate, IsOptional, IsNotEmpty, Length } from 'class-validator';
+
+export class CreateUserDto {
+    @IsEnum(UserLevelEnum)
+    user_level: UserLevelEnum;
+
+    @Length(1, 255) 
+    @IsNotEmpty()
+    @IsString()
+    last_name: string;
+
+    @Length(1, 255) 
+    @IsNotEmpty()
+    @IsString()
+    first_name: string;
+        
+    @IsEnum(GenderEnum)
+    gender: GenderEnum;
+    
+    @Length(1, 255) 
+    @IsNotEmpty()
+    @IsString()
+    address: string;
+
+    @IsDate()
+    birth_date: Date;
+
+    @Length(1, 255) 
+    @IsNotEmpty()
+    @IsString()
+    contact_no: string;
+
+    @IsNotEmpty()
+    @IsString()
+    blood_type: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+
+    @IsEnum(UserStatusEnum)
+    status: UserStatusEnum;
+
+    @IsEnum(DispatchStatusEnum)
+    dispatch_status: DispatchStatusEnum;
+
+    @IsEnum(UserTypeEnum)
+    type: UserTypeEnum;
+
+    @IsOptional()
+    @IsString()
+    bart_id?: string;
+
+    @IsOptional()
+    @IsString()
+    cso_id?: string;
+
+    @IsOptional()
+    @IsString()
+    po_id?: string;
+
+    @IsOptional()
+    @IsString()
+    na_id?: string;
+}
