c0ae8dac2d658f1f1890fc3a461dd9dc334a8206
William Jay Inclino <wjay.inclino@gmail.com>
2023-11-10 08:27:32 +0800

    ref #002: implement update user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,24 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto';
+import { CreateUserDto, UpdateUserDto } from './dto';
+import { User } from '@prisma/client';
 
 @Controller('/api/v1/user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+	constructor(private readonly userService: UserService) {}
 
-    @Delete('/truncate')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    async truncate(): Promise<void> {
-        await this.userService.truncate();
-    }
+	@Delete('/truncate')
+	@HttpCode(HttpStatus.NO_CONTENT)
+	async truncate(): Promise<void> {
+		await this.userService.truncate();
+	}
 
-    @Post()
-    create(@Body() createUserDto: CreateUserDto) {
-        return this.userService.create(createUserDto);
-    }
+	@Post()
+	@UsePipes(new ValidationPipe())
+	async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+		return await this.userService.create(createUserDto);
+	}
 
-    @Get()
-    findAll() {
-        return this.userService.findAll();
-    }
+	@Get()
+	async findAll(): Promise<User[]> {
+		return await this.userService.findAll();
+	}
+
+	@Get(':id')
+	async findOne(@Param('id') id: string): Promise<User> {
+		const user = await this.userService.findOne(id);
+		return user;
+	}
+
+	@Patch(':id')
+	@UsePipes(new ValidationPipe())
+	async update(
+		@Param('id') id: string,
+		@Body() updateUserDto: UpdateUserDto
+	): Promise<User> {
+		const updatedUser = await this.userService.update(id, updateUserDto);
+		return updatedUser;
+	}
+
+	@Delete(':id')
+	@HttpCode(HttpStatus.NO_CONTENT)
+	async remove(@Param('id') id: string): Promise<void> {
+		await this.userService.remove(id);
+	}
 }
